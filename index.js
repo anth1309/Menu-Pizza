a82734cfbf455080e743519ea7ca5b4e5b187d4e
Anthony CHAMBON <acdf-ac.cdf@hotmail.fr>
2022-11-27 18:13:21 +0100

    calcul plus moins ok avec - imp sous o manque paiement

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
   let commande = new Map();
   selection.forEach((pizza, i) => {
     commande.set(pizza.dataset.type, 0);
+
     pizza.querySelector(".plus").addEventListener("click", () => {
       pizza.querySelector(".quantite").innerHTML++;
       pizza.querySelector(".prixLigne").innerHTML =
-        pizza.querySelector(".quantite").innerHTML * pizza.dataset.prix + "€";
-      document.querySelector(".total").value = Number(pizza.dataset.prix) + "€";
-      console.log(document.querySelector(".total").value);
+        pizza.querySelector(".quantite").innerHTML * pizza.dataset.prix;
+      document.querySelector(".total").innerHTML =
+        Number(document.querySelector(".total").innerHTML) +
+        Number(pizza.dataset.prix);
+    });
+
+    pizza.querySelector(".moins").addEventListener("click", () => {
+      if (pizza.querySelector(".quantite").innerHTML > 0) {
+        pizza.querySelector(".quantite").innerHTML--;
+        pizza.querySelector(".prixLigne").innerHTML =
+          pizza.querySelector(".quantite").innerHTML * pizza.dataset.prix;
+        document.querySelector(".total").innerHTML =
+          Number(document.querySelector(".total").innerHTML) -
+          Number(pizza.dataset.prix);
+      }
     });
   });
 });
